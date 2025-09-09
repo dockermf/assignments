5cd8b8dc617ffa35d5e72b50c9567ccc28b97133
dockermf <lmao6969dock@gmail.com>
2025-09-09 21:28:30 +0500

    fix(task1.c): correct code

--- task1.c
+++ task1.c
@@ -1,21 +1,14 @@
-#include <stdio.h>
-
-#define PI 3.14159265358979323846
+#include <math.h>
 
 int main()
 {
 	float r = 5;
 	float circumference, area, surface_area, volume;
 
-	circumference = 2 * PI * r;
-	area = PI * (r * r);
-	surface_area = 4 * PI * (r * r);
-	volume = (4 * PI * (r * r * r)) / 3;
-
-	printf("Длина: %.2f\n", circumference);
-	printf("Площадь: %.2f\n", area);
-	printf("Площадь поверхности сферы: %.2f\n", surface_area);
-	printf("Объем шара: %.2f\n", volume);
-
+	circumference = 2 * M_PI * r;
+	area = M_PI * pow(r, 2);
+	surface_area = 4 * M_PI * area;
+	volume = (4 * M_PI * pow(r, 3)) / 3;
+	
 	return 0;
 }
